c9809d0d26583ea6e4cfd6f91844e25a39c1307d
Tiago Diaz <tiagoadp@gmail.com>
2017-08-29 12:05:11 -0300

    Validação de CNPJ e CPP

---
+++ bundles/custom-validator.d.ts
@@ -0,0 +1,4 @@
+import { FormControl } from "@angular/forms/forms";
+export declare class NgxCustomValidator {
+    static cpf(control: FormControl) : any
+}
---
+++ ngx-custom-form-validators.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var custom_validator_1 = require("./app/custom-validator");
+exports.NgxCustomValidator = custom_validator_1.NgxCustomValidator;
+//# sourceMappingURL=ngx-custom-form-validators.js.map
